dd557fd6f53c0ed06e7e6a71cde56dcef6764cdf
InsanoGameplays <Gabrielbatistadutra4@gmail.com>
2023-03-02 01:19:13 -0300

    teste6.0

--- js/game.js
+++ js/game.js
@@ -16,12 +16,31 @@ const characters = [
 ]
 let firstCard = '';
 let secondCard = '';
+const resetGame = () => {
+    // Remove todas as cartas da grid
+    while (grid.firstChild) {
+      grid.removeChild(grid.firstChild);
+    }
+  
+    // Resetar as variáveis de controle do jogo
+    firstCard = null;
+    secondCard = null;
+    isChecking = false;
+    disabledCards = [];
+    timer = 0;
+  
+    // Carregar um novo jogo
+    loadGame();
+  };
 
 const checkEndGame = () =>{
     const disabledCards = document.querySelectorAll('.disabled-card');
       if (disabledCards.length == 20){
        clearInterval(this.loop);
        alert(`Parabéns, ${spanPlayer.innerHTML} , seu tempo foi de: ${timer.innerHTML}!`);
+
+        // Reiniciar o jogo quando o jogador vencer
+    resetGame();
     }
 }
 
